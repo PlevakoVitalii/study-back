8aa096b8626dfa4850cca2a3f1196ad822e54e8e
Vitalii Plevako <vitalii.plevako@plus4u.net>
2023-06-18 13:34:31 +0300

    add validation and error handler to back

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -11,15 +13,12 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
 
 let notes = [
   {
@@ -51,49 +50,85 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.filter(note => note.id === id)
-  if (note.length) {
-    response.json(note)
-  } else {
-    response.status(400).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-  response.status(204).end()
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json(({ error: error.message }))
+  }
+
+  next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+// if (process.env.length < 3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
+
+// const password = process.argv[2]
+
+const url = `mongodb+srv://Vitalii113377:${password}@cluster0.p7albi1.mongodb.net/full-stack-db?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'FullStack is Easy!!!',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved:', `${result}`)
+//   mongoose.connection.close()
+// })
+
+
+Note.find({ _id: "648981637882b05efb76fe7e" }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
